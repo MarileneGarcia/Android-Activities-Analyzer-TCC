72edec512a271e2b203a5c8498230e1f77028408
MarileneGarcia <marilenegarcia1998@gmail.com>
2022-02-20 21:51:18 -0300

    vector of class

--- tcc.cpp
+++ tcc.cpp
@@ -7,20 +7,68 @@
 #include <cstdlib>
 
 using namespace std;
-//vector<string> read_logs();
-vector<string> pick_functions();
-vector<string> read_functions();
-vector<string> select_functions(vector<string> functions);
+
+typedef struct Events{
+    string event;   
+    Events* next; 
+} Events;
 
 class logFunctions { 
     private:
         string name;
-        map<int, string> events;
+        Events* event_head;
+        Events* event_tail; 
+        
+        /* thinking about pid and tid later
+        //int pid; 
+        //int tid; */
+
     public:           
         logFunctions(string function) {     
         name = function;
+        event_head = NULL;
+        event_tail = NULL;
+        }
+
+        string get_name(){
+            return name;
+        }
+
+        void add_evento(string event) {
+            if(event_head == NULL){
+                event_head = (Events*) malloc (sizeof(Events));
+                if(event_head == NULL) {
+                    cout << "Error to create a first event of Function: " + name << endl;
+                    exit(0);
+                }
+                event_head -> event = event;
+                event_head -> next = NULL;
+                event_tail = event_head;
+            } else {
+                event_tail -> next = (Events*) malloc (sizeof(Events));
+                if(event_tail -> next == NULL) {
+                    cout << "Error to create an event of Function: " + name << endl;
+                    exit(0);
+                }
+                event_tail -> next -> event = event;
+                event_tail -> next -> next = NULL;
+                event_tail = event_tail -> next;
+            }
+        }
+
+        void print_all_events(){
+            int i = 0;
+            while (event_tail -> next != NULL){
+                cout << "Message " << i << ": " << event_tail -> event << endl; 
+                i++;
+            }
         }
 };
+
+//vector<string> read_logs();
+vector<string> pick_functions();
+vector<string> read_functions();
+vector<logFunctions> select_functions(vector<string> functions);
 
 int main (){
     //vector<string> lines = read_logs();
@@ -73,7 +121,21 @@ vector<string> read_functions(){
     }
 }
 
-vector<string> select_functions(vector<string> functions){
+vector<logFunctions> select_functions(vector<string> functions){
+    vector<logFunctions> events;
+    string selection;
+    int i = 0;
     cout << "Type 'y' to select a function for analyze, or type 'n' to unselect the function" << endl;
-    for
+    for (string function : functions){
+        cout << "Do you want to analyse: \033[1;31m" + function + "\033[0m ?";
+        cin >> selection;
+
+        if(selection == "y"){
+            cout << "Do you wanna select" + function << endl;
+            events.push_back(logFunctions(function));
+            cout << events[i].get_name() << endl;
+            i++;
+        }
+    }
+    return events;
 }
