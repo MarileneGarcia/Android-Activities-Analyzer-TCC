435486fd15b2a310647faba3136dc949a1e8dedc
MarileneGarcia <marilenegarcia1998@gmail.com>
2022-04-25 13:28:01 -0300

    third screen

--- interface.py
+++ interface.py
@@ -5,50 +5,7 @@ import tkinter as tk
 from tkinter import messagebox
 import os
 
-# 2. The begining
-def begining (root, old_frame, user_choices):
-	old_frame.destroy()
-	frame = tk.LabelFrame(root, relief = FLAT, background="#86acac")
-	frame.grid(row = 0, column = 1, columnspan = 2, sticky = tk.NSEW)
-	frame.grid_columnconfigure(0, weight=1)
-	frame.grid_columnconfigure(1, weight=1)
-	frame.grid_columnconfigure(2, weight=1)
-
-	label = Label(frame, text ='What is the cellphone Brand', font = ("Courier", 18), background="#86acac") 
-	label.grid(row = 0, column = 0, sticky = N, ipady = 10)
-	choice = StringVar(frame, "none")
-
-	button_0 = tk.Button(frame, background="#86acac", font = ("Courier", 14), text = 'Select', command = lambda : first_step(choice.get(), user_choices, frame))
-	button_1 = tk.Radiobutton(frame, background="#86acac", font = ("Courier", 14), text = mot, variable = choice, value = mot)
-	button_2 = tk.Radiobutton(frame, background="#86acac", font = ("Courier", 14), text = smsng, variable = choice, value = smsng)
-	button_3 = tk.Radiobutton(frame, background="#86acac", font = ("Courier", 14), text = xm, variable = choice, value = xm)
-	button_4 = tk.Radiobutton(frame, background="#86acac", font = ("Courier", 14), text = lg, variable = choice, value = lg)
-
-	button_0.grid(row = 5, column = 0, sticky = N, pady = 2, padx = 10)
-	button_1.grid(row = 1, column = 0, sticky = NW, pady = 2, padx = 10, ipady = 1)  
-	button_2.grid(row = 2, column = 0, sticky = NW, pady = 2, padx = 10, ipady = 1)
-	button_3.grid(row = 3, column = 0, sticky = NW, pady = 2, padx = 10, ipady = 1)
-	button_4.grid(row = 4, column = 0, sticky = NW, pady = 2, padx = 10, ipady = 1)
-
-
-
-# 3. The first step, a choice of brand
-def first_step(mensagem, user_choices, frame): 
-	if mensagem  != "none" :
-		command_dir = ""
-		user_choices.append(mensagem)
-		for choice in user_choices :
-			command_dir = command_dir + mensagem + "/"
-		messagebox.showinfo('pop-up information', 'You select: ' + mensagem)
-		command = "./saida.out 1 " + command_dir
-		stream = os.popen(command)
-		output = stream.read()
-		if "program error" in output:
-			#must need to handle with the error
-			messagebox.showinfo('pop-up error', str(output))
-			print(str(output))
-		frame.destroy()
-
+# 1. Building First Screen and Root
 # create root window
 root = Tk()
 
@@ -82,10 +39,88 @@ frame.grid_columnconfigure(2, weight=1)
 label = tk.Label(frame, relief = FLAT, text ='Do you want to start Android Activities Analyzer', font = ("Courier", 18), background="#86acac") 
 label.grid(row = 0, column = 0, sticky = N, ipady = 10)
 
-button = tk.Button(frame, background="#86acac", font = ("Courier", 14), text = 'Begin', command = lambda : begining(root, frame, user_choices))
+button = tk.Button(frame, background="#86acac", font = ("Courier", 14), text = 'Begin', command = lambda : second_screen(root, frame, user_choices))
 button.grid(row = 1, column = 0)
 
+# 2. Building Second Screen
+def second_screen (root, old_frame, user_choices):
+	old_frame.destroy()
+	frame = tk.LabelFrame(root, relief = FLAT, background="#86acac")
+	frame.grid(row = 0, column = 1, columnspan = 2, sticky = tk.NSEW)
+	frame.grid_columnconfigure(0, weight=1)
+	frame.grid_columnconfigure(1, weight=1)
+	frame.grid_columnconfigure(2, weight=1)
 
+	label = Label(frame, text ='What is the cellphone Brand', font = ("Courier", 18), background="#86acac") 
+	label.grid(row = 0, column = 0, sticky = N, ipady = 10)
+	choice = StringVar(frame, "")
+
+	button_0 = tk.Button(frame, background="#86acac", font = ("Courier", 14), text = 'Select', command = lambda : first_step_algorithm(choice.get(), user_choices, frame))
+	button_1 = tk.Radiobutton(frame, background="#86acac", font = ("Courier", 14), text = mot, variable = choice, value = mot)
+	button_2 = tk.Radiobutton(frame, background="#86acac", font = ("Courier", 14), text = smsng, variable = choice, value = smsng)
+	button_3 = tk.Radiobutton(frame, background="#86acac", font = ("Courier", 14), text = xm, variable = choice, value = xm)
+	button_4 = tk.Radiobutton(frame, background="#86acac", font = ("Courier", 14), text = lg, variable = choice, value = lg)
+
+	button_0.grid(row = 5, column = 0, sticky = N, pady = 2, padx = 10)
+	button_1.grid(row = 1, column = 0, sticky = NW, pady = 2, padx = 10, ipady = 1)  
+	button_2.grid(row = 2, column = 0, sticky = NW, pady = 2, padx = 10, ipady = 1)
+	button_3.grid(row = 3, column = 0, sticky = NW, pady = 2, padx = 10, ipady = 1)
+	button_4.grid(row = 4, column = 0, sticky = NW, pady = 2, padx = 10, ipady = 1)
+
+
+# 3. The choice of brand
+def first_step_algorithm (mensagem, user_choices, frame): 
+	# Verification if some brand was select
+	if mensagem  != "" :
+		command_dir = ""
+		user_choices.append(mensagem)
+		for choice in user_choices :
+			command_dir = command_dir + choice + "/"
+		if messagebox.askyesno('pop-up information', 'You select: ' + mensagem) :
+			command = "./saida.out 1 " + command_dir
+			stream = os.popen(command)
+			output = stream.read()
+			if "program error" in output:
+				#must need to handle with the error
+				messagebox.showerror('pop-up error', str(output))
+			print(user_choices)
+			third_screen(root, frame, user_choices)
+
+
+# 3. Building Third Screen
+def third_screen (root, old_frame, user_choices):
+	old_frame.destroy()
+	frame = tk.LabelFrame(root, relief = FLAT, background="#86acac")
+	frame.grid(row = 0, column = 1, columnspan = 2, sticky = tk.NSEW)
+	frame.grid_columnconfigure(0, weight=1)
+	frame.grid_columnconfigure(1, weight=1)
+	frame.grid_columnconfigure(2, weight=1)
+
+	label = Label(frame, text ='What is the cellphone Model', font = ("Courier", 18), background="#86acac") 
+	label.grid(row = 0, column = 0, sticky = N, ipady = 10)
+	choice = StringVar(frame, "")
+	entry = tk.Entry(frame, textvariable = choice, font=('calibre',10,'normal'))
+	button = tk.Button(frame, background="#86acac", font = ("Courier", 14), text = 'Select', command = lambda : second_step_algorithm(choice.get(), user_choices, frame))
+	button.grid(row = 2, column = 0, sticky = N, pady = 2, padx = 10)
+	entry.grid(row = 1, column = 0, sticky = N, pady = 2, padx = 10, ipady = 5)  
+	
+# 3. The choice of model
+def second_step_algorithm (mensagem, user_choices, frame): 
+	# Verification if some brand was select
+	if mensagem  != "" :
+		command_dir = ""
+		user_choices.append(mensagem)
+		for choice in user_choices :
+			command_dir = command_dir + choice + "/"
+		if messagebox.askyesno('pop-up information', 'You select: ' + mensagem) :
+			command = "./saida.out 1 " + command_dir
+			stream = os.popen(command)
+			output = stream.read()
+			if "program error" in output:
+				#must need to handle with the error
+				messagebox.showerror('pop-up error', str(output))
+				print(str(output))
+			third_screen(root, frame, user_choices)
 
 
 
